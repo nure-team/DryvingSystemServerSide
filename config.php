05345c900c6c698f34b8c2376ce597bc95494be6
annndre <102580124+annndre@users.noreply.github.com>
2023-05-02 21:39:00 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once('google-api/vendor/autoload.php');
 
 $gClient = new Google_Client();
-$gClient->setClientId("321320177910-cqigd4kvtmglmicd2m1ljasbn45pc946.apps.googleusercontent.com");
-$gClient->setClientSecret("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");
+$gClient->setClientId(".apps.googleusercontent.com");
+$gClient->setClientSecret("");
 
 $gClient->setApplicationName("DrivingSystem");
 $gClient->setRedirectUri("http://localhost/login/controller.php");
