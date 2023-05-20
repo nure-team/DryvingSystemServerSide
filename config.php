55c876d1fabff7d8829a1336bee0d745a8c9c691
annndre <andr.sosn@gmail.com>
2023-05-20 22:48:26 +0300

    updating config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 const CLASSES_DIR = __DIR__. '/login/core/';
 const USER_DIR = __DIR__. '/login/';
+const PAYMENT_DIR = __DIR__. '/vendor/';
 
 require_once('google-api/vendor/autoload.php');
 
@@ -8,8 +9,8 @@ $gClient = new Google_Client();
 
 $gClient->setClientId("321320177910-cqigd4kvtmglmicd2m1ljasbn45pc946.apps.googleusercontent.com");
 $gClient->setClientSecret("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");
-$gClient->setApplicationName("Driving");
 
+$gClient->setApplicationName("Driving");
 $gClient->setRedirectUri("http://localhost/login/controller.php");
 
 $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
