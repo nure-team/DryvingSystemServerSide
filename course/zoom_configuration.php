a3b8832fffa748517de2e9283f8a7de801c6f806
annndre <andr.sosn@gmail.com>
2023-05-30 21:20:39 +0300

    Creating admin page

---
+++ course/zoom_configuration.php
@@ -0,0 +1,7 @@
+<?php
+define('API_KEY', 'NOvmfeb8R7S0GCXwmnThqw');
+define('API_SECRET', '3mPMqxE5WyDCYexII6nBqSltgisfEtpTdUv9');
+define('EMAIL_ID', 'andr.sosn@gmail.com');
+//NOvmfeb8R7S0GCXwmnThqw
+//3mPMqxE5WyDCYexII6nBqSltgisfEtpTdUv9
+//andr.sosn@gmail.com
